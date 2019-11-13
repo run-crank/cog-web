7c751d2a6682f3f58b28fb7e2c1317528dcee109
Andrei <apanaligan@you-source.com>
2019-11-13 18:10:58 +0800

    Added support for iframe by implementing FocusToFrame step

---
+++ src/steps/focus-on.frame.ts
@@ -0,0 +1,33 @@
+import { BaseStep, Field, StepInterface } from '../core/base-step';
+import { Step, RunStepResponse, FieldDefinition, StepDefinition } from '../proto/cog_pb';
+
+export class FocusOnFrame extends BaseStep implements StepInterface {
+
+  protected stepName: string = 'Focus on Frame';
+  // tslint:disable-next-line:max-line-length
+  protected stepExpression: string = 'focus on the (?<domQuerySelector>.+) frame';
+  protected stepType: StepDefinition.Type = StepDefinition.Type.ACTION;
+  protected expectedFields: Field[] = [{
+    field: 'domQuerySelector',
+    type: FieldDefinition.Type.STRING,
+    description: 'The iframe\'s DOM query selector, or "main" for the main frame',
+  }];
+
+  async executeStep(step: Step): Promise<RunStepResponse> {
+    const stepData: any = step.getData().toJavaScript();
+    const iframeSelector: string = stepData.domQuerySelector;
+
+    try {
+      await this.client.focusFrame(iframeSelector);
+      return this.pass('Successfully focused on frame %s', [iframeSelector]);
+    } catch (e) {
+      return this.error('Unable to focus on frame %s, the frame may no longer exist on the page: %s', [
+        iframeSelector,
+        e.toString(),
+      ]);
+    }
+  }
+
+}
+
+export { FocusOnFrame as Step };
