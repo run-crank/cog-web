aaf8e2fb33892d55708adb3b1b092720de2e6fbf
Angelito Salud <asalud@you-source.com>
2020-02-11 15:14:51 +0800

    fixed filterGoogleAdsURL

--- src/client/mixins/google-ads.ts
+++ src/client/mixins/google-ads.ts
@@ -1,9 +1,9 @@
 export class GoogleAdsAware {
   filterGoogleAdsURLs(requests, aid, group, atag) {
     const filteredUrl = requests.filter(r => r.url.includes('ad.doubleclick.net') || r.url.includes('fls.doubleclick.net')).map(request => decodeURIComponent(request.url));
-    const result = filteredUrl.filter(url => url.includes(`src=${aid}`)
-                                  && url.toLowerCase().includes(`type=${group.toLowerCase()}`)
-                                  && url.toLowerCase().includes(`cat=${atag.toLowerCase()}`));
+    const result = filteredUrl.filter(url => url.includes(`src=${aid};`)
+                                  && url.toLowerCase().includes(`type=${group.toLowerCase()};`)
+                                  && url.toLowerCase().includes(`cat=${atag.toLowerCase()};`));
     return result;
   }
   getParameters(url) {
