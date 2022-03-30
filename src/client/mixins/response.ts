1fbac21cf8e5a1950048004e4c14c29255f095ff
saludangelito <asalud@you-source.com>
2022-03-30 18:37:46 +0800

    changed check current page info to check current frame instead of lastResponse

--- src/client/mixins/response.ts
+++ src/client/mixins/response.ts
@@ -7,20 +7,21 @@ export class ResponseAware {
    */
   public async getCurrentPageInfo(detail: string): Promise<String> {
     // Immediately throw an error if we don't have a response stashed.
-    if (!this.client['___lastResponse']) {
+    if (!this.client['___currentFrame']) {
       throw new Error('No page context. Ensure this step is preceded by a page navigation step.');
     }
 
     if (detail === 'url') {
-      return await this.client['___lastResponse']['url']();
+      console.log(this.client);
+      return await this.client['___currentFrame']['url']();
     }
 
     if (detail === 'text') {
-      return await this.client['___lastResponse']['text']();
+      return await this.client['___currentFrame']['text']();
     }
 
     if (detail === 'status') {
-      return await this.client['___lastResponse']['status']();
+      return await this.client['___currentFrame']['status']();
     }
 
     throw new Error(`Unknown page detail: ${detail}. Should be one of: url, text, or status.`);
