c5013ce468a9680df537ba1cbe8bd218aedc75d6
Austin Elwell <austinelwell99@gmail.com>
2022-07-25 17:20:21 -0500

    added timed logs to help debug in prod

--- src/steps/navigate-to-page.ts
+++ src/steps/navigate-to-page.ts
@@ -19,10 +19,16 @@ export class NavigateToPage extends BaseStep implements StepInterface {
 
     // Navigate to URL.
     try {
+      console.time('time');
+      console.log('>>>>> STARTED TIMER FOR NAVIGATE-TO-PAGE STEP');
       await this.client.navigateToUrl(url);
       const screenshot = await this.client.client.screenshot({ type: 'jpeg', encoding: 'binary', quality: 60 });
       const binaryRecord = this.binary('screenshot', 'Screenshot', 'image/jpeg', screenshot);
+      console.log('>>>>> checkpoint 6: finished taking screenshot and making binary record');
+      console.timeLog('time');
       const status = await this.client.client['___lastResponse']['status']();
+      console.log('>>>>> checkpoint 7: finished getting status, ending timer');
+      console.timeEnd('time');
       if (status === 404) {
         return this.fail('%s returned an Error: 404 Not Found', [url], [binaryRecord]);
       }
