c969858ebe517e65d5e96f005dd3cdd1f8f70bfe
Russell Hart <russell.hart@stackmoxie.com>
2024-03-07 16:04:08 -0500

    Updated dockerfile and puppeteer to use the installed chrome when in docker

--- src/core/grpc-server.ts
+++ src/core/grpc-server.ts
@@ -49,6 +49,7 @@ async function instantiateCluster(): Promise<Cluster> {
     retryDelay: process.env.hasOwnProperty('CLUSTER_RETRY_DELAY_MS') ? Number(process.env.CLUSTER_RETRY_DELAY_MS) : 3000,
     timeout: process.env.hasOwnProperty('CLUSTER_TIMEOUT_MS') ? Number(process.env.CLUSTER_TIMEOUT_MS) : 900000,
     puppeteerOptions: {
+      ...(process.env.IN_DOCKER && {executablePath: '/usr/bin/google-chrome'}), // adds executablePath only in docker
       headless: false,
       args: process.env.IN_DOCKER ? [
         '--no-sandbox',
