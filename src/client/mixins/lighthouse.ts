41c0013f5b2a7af471a8c7159e59ad9416b82a80
Russell Hart <russell.hart@stackmoxie.com>
2024-09-09 17:19:02 -0400

    Lighthouse - Fix user agent

--- src/client/mixins/lighthouse.ts
+++ src/client/mixins/lighthouse.ts
@@ -21,13 +21,9 @@ export class LighthouseAware {
 
      // Set the User-Agent based on the throttleTo value
      if (throttleTo === 'mobile') {
-      config.settings.extraHeaders = {
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1 AutomatonChrome'
-      };
+      config.settings.emulatedUserAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1 AutomatonChrome';
     } else {
-      config.settings.extraHeaders = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 AutomatonChrome'
-      };
+      config.settings.emulatedUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) AutomatonChrome/91.0.4472.124 Safari/537.36';
     }
 
     let { lhr } = await this.lighthouse(url, flags, config);
